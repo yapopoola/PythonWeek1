de75675baf6a892b899572191c2b7bfc52931b60
rafi <rafihatu.bello@barbora.lt>
2025-07-25 23:57:19 +0300

    added stringz.py and modified numberz.py

---
+++ stringz.py
@@ -0,0 +1,24 @@
+# Strings can be defined using single quotes as below
+student_name = 'Yusuf Popoola'
+
+# Strings can be defined using double quotes as below
+teacher_name = "Rafihatu Bello"
+
+# Strings can be spread into multiple lines using three of either double or single quotes as below
+class_description = '''
+    This is just a small example.
+    The student is here.
+    The teacher is also here.
+'''
+
+encapsulated_string = (
+"This is aunty B"
+"This is also uncle B"
+"Madam C dey here o"
+"UNcle C too dey"
+)
+
+# Accessing attributes of strings using dot notation
+teacher_and_student = "The teacher's name is: {} while the student's name is: {}".format(teacher_name, student_name)
+password = "7a"
+print(password.isalnum())
